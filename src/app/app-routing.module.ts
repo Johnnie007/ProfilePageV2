6b84ddea815c080acbd1a349565eaa1603205672
Johnnie432 <johnnie432@icloud.com>
2024-01-13 18:39:44 -0500

    built portfolio page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 
 const routes: Routes = [
-  {path: '', component: HomepageComponent}
+  {path: '', component: HomepageComponent},
+  {path: 'portfolio', component: PortfolioPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    PortfolioPageComponent
   ],
   imports: [
     BrowserModule,
