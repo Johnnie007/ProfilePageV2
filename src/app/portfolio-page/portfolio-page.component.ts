ae1d1bcdd99036f1c0cf59f54303e95400855560
Johnnie432 <johnnie432@icloud.com>
2024-01-13 21:23:58 -0500

    added fonts

--- src/app/portfolio-page/portfolio-page.component.ts
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio-page.component.scss']
 })
 export class PortfolioPageComponent {
+  projects = [
+   {
+    "name": "Footprint",
+    "description": "Monthly Carbon Output Tracker.",
+    "stack": "Angular, SpringBoot, MySQL",
+    "languages": "Typescript, Java, SQL",
+    "link": "https://johnnie007.github.io/FootPrintV2Frontend/#/"
+   }, 
+   {
+    "name": "Mosaic Me",
+    "description": "Converts any image into a Mosaic",
+    "stack": "React",
+    "languages": "Javascript",
+    "link": "https://johnnie007.github.io/mosaic-me/#/"
+   }, 
+   {
+    "name": "Archive",
+    "description": "Various projects that were built using differnt types of langauages",
+    "stack": "Varies",
+    "languages": "Varies",
+    "link": "https://github.com/Johnnie007?tab=repositories"
+   }, 
+  ];
+
+  mainIndex = 0;
+  secondIndex = 1;
+  thirdIndex = 2;
+
+  counterClockWise(){
+    let holder = this.mainIndex;
+    this.mainIndex = this.secondIndex;
+    this.secondIndex = this.thirdIndex;
+    this.thirdIndex = holder;
+  }
+
+  clockWise(){
+    let holder = this.mainIndex;
+    this.mainIndex = this.thirdIndex;
+    this.thirdIndex = this.secondIndex
+    this.secondIndex = holder;
+  }
 
 }
